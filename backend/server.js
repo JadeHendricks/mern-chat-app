0c8bc7b848fb7f7d3e4b87f7dbc1c396e8ae0f00
Jade <hendricksjade@gmail.com>
2022-02-04 22:38:59 +0200

    adding in mongo_uri string and connecting to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,24 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const { chats } = require('./data/data');
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+const connectDB = require("./config/db");
 
 const app = express();
 dotenv.config();
 
-app.get('/', (req, res) => {
-    res.send('API is running');
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("API is running");
 });
 
-app.get('/api/chat', (req, res) => {
-    res.send(chats);
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
 });
 
-app.get('/api/chat/:id', (req, res) => {
-    const singleChat = chats.find(chat => chat._id === req.params.id);
-    res.send(singleChat);
+app.get("/api/chat/:id", (req, res) => {
+  const singleChat = chats.find((chat) => chat._id === req.params.id);
+  res.send(singleChat);
 });
 
 const PORT = process.env.PORT || 5000;
