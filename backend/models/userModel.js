96ef968b9cfabd52fa8abcbc848529473ce03c61
Jade <hendricksjade@gmail.com>
2022-02-05 00:01:04 +0200

    adding in auto endpoints for register and login,a dded in error handling middleware

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,30 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const userModel =  mongoose.Schema(
-    {
-        name: { 
-            type: String, 
-            required: true 
-        },
-        email: { 
-            type: String, 
-            required: true 
-        },
-        password: { 
-            type: String, 
-            required: true 
-        },
-        picture: { 
-            type: String, 
-            required: true, 
-            default: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg" 
-        }
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true
-    }
-)
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    picture: {
+      type: String,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 //Create Model = name of the model + model
-const User = mongoose.model("User", userModel);
+const User = mongoose.model("User", userSchema);
 module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -2,24 +2,24 @@ const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoute");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
 dotenv.config();
 
 connectDB();
 
+//need this for the backend to accept JSON
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
-
-app.get("/api/chat/:id", (req, res) => {
-  const singleChat = chats.find((chat) => chat._id === req.params.id);
-  res.send(singleChat);
-});
+app.use("/api/user", userRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
